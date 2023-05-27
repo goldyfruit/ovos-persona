7d4cc3ab22a4afd31a1ac8f21d327e2782f56621
JarbasAi <jarbasai@mailfence.com>
2023-05-27 19:02:27 +0100

    intents + context + converse state management

--- ovos_persona/skill.py
+++ ovos_persona/skill.py
@@ -1,8 +1,11 @@
 import os
 
 from ovos_utils import classproperty
+from ovos_utils.intents import IntentBuilder
+from ovos_utils.log import LOG
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_utils.xdg_utils import xdg_data_home
+from ovos_workshop.decorators import intent_handler, adds_context, removes_context
 from ovos_workshop.skills.fallback import FallbackSkill
 
 from ovos_persona import PersonaService
@@ -27,16 +30,17 @@ class PersonaSkill(FallbackSkill):
         personas_folder = f"{xdg_data_home()}/personas"
         os.makedirs(personas_folder, exist_ok=True)
         self.persona = PersonaService(personas_folder)
-
-        # choose default persona, filename without .json extension
-        self.active_persona = self.settings.get("default_persona")
+        self.active_persona = None
 
         self.add_event("ovos.persona.register", self.handle_register_persona)
         self.add_event("ovos.persona.deregister", self.handle_deregister_persona)
         self.add_event("ovos.persona.enable", self.handle_enable_persona)
         self.add_event("ovos.persona.disable", self.handle_disable_persona)
+        self.add_event("ovos.persona.ask", self.handle_ask_persona)
+
         self.register_fallback(self.ask_persona, 85)
 
+    # bus api
     def handle_register_persona(self, message):
         name = message.data.get("name")
         persona = message.data.get("persona")
@@ -46,18 +50,55 @@ class PersonaSkill(FallbackSkill):
         name = message.data.get("name")
         self.persona.deregister_persona(name)
 
+    @adds_context("ActivePersona")
     def handle_enable_persona(self, message):
-        name = message.data.get("name")
-        self.active_persona = name  # TODO Session support
+        self.active_persona = message.data.get("name")  # TODO Session support (from context)
 
+    @removes_context("ActivePersona")
     def handle_disable_persona(self, message):
         self.active_persona = None
 
-    def ask_persona(self, message):
-        if self.active_persona is None:
-            return False
+    def handle_ask_persona(self, message):
         utterance = message.data['utterance']
         answer = self.persona.chatbox_ask(utterance, self.active_persona, self.lang)
+        self.bus.emit(message.response(data={"utterance": answer, "lang": self.lang}))
+
+    # intents
+    # NB: adapt intents requiring ActivePersona keyword,
+    #     that keyword is virtual, it is only set/removed in converse method
+    @intent_handler("summon.intent")
+    def handle_summon_persona(self, message):
+        self.handle_enable_persona(message)
+
+    @intent_handler(IntentBuilder("ReleasePersona")
+                    .require("ActivePersona")
+                    .require("Release"))
+    def handle_release_persona(self, message):
+        self.handle_disable_persona(message)
+
+    # converse
+    def handle_deactivate(self, message):
+        """ skill is no longer considered active by the intent service
+        converse method will not be called, skills might want to reset state here
+        """
+        if self.active_persona:
+            LOG.info(f"converse timed out, deactivating: {self.active_persona}")
+            self.handle_disable_persona(message)
+
+    def converse(self, message=None):
+        if self.active_persona:
+            # TODO - match handle_release_persona intent
+            #
+            return self.ask_persona(message)
+        return False
+
+    # fallback
+    def ask_persona(self, message):
+        persona = self.active_persona or self.settings.get("default_persona")
+        if persona is None:
+            return False
+        utterance = message.data['utterance']
+        answer = self.persona.chatbox_ask(utterance, persona, self.lang)
         if not answer:
             return False
         self.speak(answer)
