62b436602a3d9809ec3fefedba4c5d9207046240
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-06-06 21:31:52 -0400

    [setup] normalize setup

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
-import os
-
 from setuptools import setup
+from os import walk, path, environ
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-PLUGIN_ENTRY_POINT = f"persona.openvoiceos=ovos_persona.skill:PersonaSkill"
+URL = "https://github.com/OpenVoiceOS/ovos-personal"
+SKILL_CLAZZ = "PersonaSkill"
+PYPI_NAME = "ovos-persona"
 
+# below derived from github url to ensure standard skill_id
+SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
+SKILL_PKG = SKILL_NAME.lower().replace("-", "_")
+PLUGIN_ENTRY_POINT = (
+    f"{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}"
+)
 
-def required(requirements_file):
-    """Read requirements file and remove comments and empty lines."""
-    with open(os.path.join(BASEDIR, requirements_file), "r") as f:
-        requirements = f.read().splitlines()
-        if "MYCROFT_LOOSE_REQUIREMENTS" in os.environ:
-            print("USING LOOSE REQUIREMENTS!")
-            requirements = [
-                r.replace("==", ">=").replace("~=", ">=") for r in requirements
-            ]
-        return [pkg for pkg in requirements if pkg.strip() and not pkg.startswith("#")]
+BASEDIR = path.abspath(path.dirname(__file__))
 
 
 def get_version():
     """Find the version of the package"""
     version = None
-    version_file = os.path.join(BASEDIR, "ovos_persona", "version.py")
+    version_file = path.join(BASEDIR, "ovos_persona", "version.py")
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
@@ -43,19 +40,52 @@ def get_version():
     return version
 
 
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(BASEDIR, requirements_filename)
+    with open(requirements_file, "r", encoding="utf-8") as r:
+        requirements = r.readlines()
+    requirements = [
+        r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")
+    ]
+    if "MYCROFT_LOOSE_REQUIREMENTS" in environ:
+        print("USING LOOSE REQUIREMENTS!")
+        requirements = [r.replace("==", ">=").replace("~=", ">=") for r in requirements]
+    return requirements
+
+
+def find_resource_files():
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex", "weather_helpers")
+    package_data = ["*.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(BASEDIR, res)):
+            for directory, _, files in walk(path.join(BASEDIR, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(BASEDIR, "").lstrip("/"), "*")
+                    )
+    return package_data
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
 setup(
-    name="ovos-persona",
+    name=PYPI_NAME,
     version=get_version(),
     description="persona ovos",
-    url="https://github.com/OpenVoiceOS/ovos-persona",
+    url=URL,
     author="jarbasai",
     author_email="jarbasai@mailfence.com",
     license="MIT",
-    packages=["ovos_persona"],
+    package_dir={SKILL_PKG: ""},
+    package_data={SKILL_PKG: find_resource_files()},
+    packages=[SKILL_PKG],
     zip_safe=True,
-    install_requires=required("requirements.txt"),
+    install_requires=get_requirements("requirements.txt"),
     long_description="ovos persona",
     long_description_content_type="text/markdown",
     include_package_data=True,
+    keywords="ovos skill plugin persona",
     entry_points={"ovos.plugin.skill": PLUGIN_ENTRY_POINT},
 )
